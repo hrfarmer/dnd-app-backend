8b5c09fb389332a93b946ec53972414d2d3242e5
aychar <58487401+hrfarmer@users.noreply.github.com>
2024-12-25 23:52:03 -0600

    clean up unused fields on discorduser

--- src/lib.rs
+++ src/lib.rs
@@ -21,17 +21,7 @@ pub struct DiscordUser {
     discriminator: String,
     global_name: Option<String>,
     avatar: Option<String>,
-    bot: Option<bool>,
-    system: Option<bool>,
-    mfa_enabled: Option<bool>,
-    banner: Option<String>,
     accent_color: Option<i32>,
-    locale: Option<String>,
-    verified: Option<bool>,
-    email: Option<String>,
-    flags: Option<i32>,
-    premium_type: Option<i32>,
-    public_flags: Option<i32>,
 }
 
 pub struct AppState {
